a9320965ff3cf81fd81f871d2b0e49ab0bb20a23
cooper <pbg0205@naver.com>
2022-07-13 18:30:42 +0900

    first commit

---
+++ src/main/java/kr/co/musinsa/cooper/musinahomework/MusinaHomeworkApplication.java
@@ -0,0 +1,13 @@
+package kr.co.musinsa.cooper.musinahomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusinaHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusinaHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/musinsa/cooper/musinahomework/MusinaHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.musinsa.cooper.musinahomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusinaHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
