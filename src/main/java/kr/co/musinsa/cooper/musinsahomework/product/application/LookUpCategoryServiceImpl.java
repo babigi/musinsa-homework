2767040c08e6a7a2c5051d4559a6ff930801238e
cooper <pbg0205@naver.com>
2022-07-17 20:00:13 +0900

    feat: LookUpCategoryService 추가

---
+++ src/main/java/kr/co/musinsa/cooper/musinsahomework/product/application/LookUpCategoryServiceImpl.java
@@ -0,0 +1,18 @@
+package kr.co.musinsa.cooper.musinsahomework.product.application;
+
+import kr.co.musinsa.cooper.musinsahomework.product.domain.LookUpCategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LookUpCategoryServiceImpl implements LookupCategoryService {
+
+    private final LookUpCategoryRepository lookUpCategoryRepository;
+
+    @Override
+    public boolean existCategory(String categoryName) {
+        return lookUpCategoryRepository.existCategory(categoryName);
+    }
+
+}
---
+++ src/main/java/kr/co/musinsa/cooper/musinsahomework/product/application/LookupCategoryService.java
@@ -0,0 +1,7 @@
+package kr.co.musinsa.cooper.musinsahomework.product.application;
+
+public interface LookupCategoryService {
+
+    boolean existCategory(String categoryName);
+
+}
