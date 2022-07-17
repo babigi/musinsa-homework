39565e724b573db9f91e5380f4c977fe5201f5ee
cooper <pbg0205@naver.com>
2022-07-17 11:38:46 +0900

    chore: sample-data.sql 추가

---
+++ src/test/resources/sample-data.sql
@@ -0,0 +1,113 @@
+-- brand
+INSERT INTO brand (name) VALUES ('A');
+INSERT INTO brand (name) VALUES ('B');
+INSERT INTO brand (name) VALUES ('C');
+INSERT INTO brand (name) VALUES ('D');
+INSERT INTO brand (name) VALUES ('E');
+INSERT INTO brand (name) VALUES ('F');
+INSERT INTO brand (name) VALUES ('G');
+INSERT INTO brand (name) VALUES ('H');
+INSERT INTO brand (name) VALUES ('I');
+
+-- category
+INSERT INTO category (name) VALUES ('top');
+INSERT INTO category (name) VALUES ('outer');
+INSERT INTO category (name) VALUES ('pants');
+INSERT INTO category (name) VALUES ('sneakers');
+INSERT INTO category (name) VALUES ('bag');
+INSERT INTO category (name) VALUES ('hat');
+INSERT INTO category (name) VALUES ('sock');
+INSERT INTO category (name) VALUES ('accessory');
+
+-- product
+
+-- A product
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('a_top', 11200, 1, 1);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('a_outer', 5500, 1, 2);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('a_pants', 4200, 1, 3);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('a_sneakers', 9000, 1, 4);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('a_bag', 2000, 1, 5);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('a_hat', 1700, 1, 6);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('a_sock', 1800, 1, 7);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('a_accessory', 2300, 1, 8);
+
+-- B product
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('b_top', 10500, 2, 1);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('b_outer', 5900, 2, 2);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('b_pants', 3800, 2, 3);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('b_sneakers', 9100, 2, 4);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('b_bag', 2100, 2, 5);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('b_hat', 2000, 2, 6);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('b_sock', 2000, 2, 7);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('b_accessory', 2200, 2, 8);
+
+-- C product
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('c_top', 10000, 3, 1);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('c_outer', 6200, 3, 2);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('c_pants', 3300, 3, 3);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('c_sneakers', 9200, 3, 4);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('c_bag', 2200, 3, 5);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('c_hat', 1900, 3, 6);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('c_sock', 2000, 3, 7);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('c_accessory', 2100, 3, 8);
+
+
+-- D product
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('d_top', 10100, 4, 1);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('d_outer', 5100, 4, 2);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('d_pants', 3000, 4, 3);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('d_sneakers', 9500, 4, 4);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('d_bag', 2500, 4, 5);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('d_hat', 1500, 4, 6);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('d_sock', 2400, 4, 7);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('d_accessory', 2000, 4, 8);
+
+-- E product
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('d_top', 10700, 5, 1);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('d_outer', 5000, 5, 2);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('d_pants', 3800, 5, 3);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('d_sneakers', 9900, 5, 4);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('d_bag', 2300, 5, 5);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('d_hat', 1800, 5, 6);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('d_sock', 2100, 5, 7);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('d_accessory', 2100, 5, 8);
+
+-- F product
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('e_top', 10700, 6, 1);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('e_outer', 5000, 6, 2);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('e_pants', 3800, 6, 3);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('e_sneakers', 9900, 6, 4);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('e_bag', 2300, 6, 5);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('e_hat', 1800, 6, 6);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('e_sock', 2100, 6, 7);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('e_accessory', 2100, 6, 8);
+
+-- G product
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('g_top', 10500, 7, 1);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('g_outer', 5800, 7, 2);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('g_pants', 3900, 7, 3);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('g_sneakers', 9000, 7, 4);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('g_bag', 2200, 7, 5);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('g_hat', 1700, 7, 6);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('g_sock', 2100, 7, 7);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('g_accessory', 2000, 7, 8);
+
+-- H product
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('h_top', 10800, 8, 1);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('h_outer', 6300, 8, 2);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('h_pants', 3100, 8, 3);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('h_sneakers', 9700, 8, 4);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('h_bag', 2100, 8, 5);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('h_hat', 1600, 8, 6);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('h_sock', 2000, 8, 7);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('h_accessory', 2000, 8, 8);
+
+-- I product
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('i_top', 11400, 9, 1);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('i_outer', 6700, 9, 2);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('i_pants', 3200, 9, 3);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('i_sneakers', 9500, 9, 4);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('i_bag', 2400, 9, 5);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('i_hat', 1700, 9, 6);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('i_sock', 1700, 9, 7);
+INSERT INTO product (name, price, brand_id, category_id) VALUES ('i_accessory', 2400, 9, 8);
